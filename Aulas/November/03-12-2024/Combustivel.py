1b2c7e1997414bcbb768a2d93f7ebfa471ad496d
r4ph4elrt5 <r4ph4elrt5@gmail.com>
2024-12-03 22:49:24 -0400

    V1.0.5
    Combustivel

---
+++ Aulas/November/03-12-2024/Combustivel.py
@@ -0,0 +1,12 @@
+print('Aqui vamos saber quanto está o litro da gasulina!')
+litros_abastecidos = int(input('Digite quantos litros você abasteceu:'))
+valor_pago = int(input('Digite quanto você pagou para abastecer:'))
+
+valor_por_litro = (valor_pago / litros_abastecidos)
+print(valor_por_litro)
+
+
+
+
+
+

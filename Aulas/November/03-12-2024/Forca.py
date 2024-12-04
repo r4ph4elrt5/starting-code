bf63db8d035391ea134a068cd55c72ad0c08a8da
r4ph4elrt5 <r4ph4elrt5@gmail.com>
2024-12-03 22:44:06 -0400

    V1.0.4
    Forca

---
+++ Aulas/November/03-12-2024/Forca.py
@@ -0,0 +1,10 @@
+import random as rd
+
+
+
+print('\nVou escolher uma das 3 palavras e você vai tentar advinha: ')
+
+lista = ['leite', 'cana', 'cama']
+letra_sortiada = rd.choice(lista)
+
+print(letra_sortiada)
